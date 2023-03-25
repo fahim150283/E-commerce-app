0f23995c1ce92143508ffb350bb4764d75014591
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-26 03:21:06 +0600

    still creating

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>1st january</div>
+            <div>
+                <h2>Birthday</h2>
+                <div>23rd</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
