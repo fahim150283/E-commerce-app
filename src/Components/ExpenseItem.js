8742142a3e00e32bc995ac3d07d2332b8a7259b0
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-26 05:00:43 +0600

    still creating

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,4 +1,4 @@
-import './ExpenseItem'
+import './ExpenseItem.css'
 
 function ExpenseItem(){
     return (
@@ -6,7 +6,7 @@ function ExpenseItem(){
             <div className="expense-item__description">1st january 2023</div>
             <div>
                 <h2>Car insurance</h2>
-                <div className="expense-item__price">42300</div>
+                <div className="expense-item__price">$ 2300</div>
             </div>
         </div>
     );
