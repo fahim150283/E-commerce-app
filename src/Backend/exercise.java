502709d04c6e7cba4bee2a3a0107f612244da8eb
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-26 02:47:16 +0600

    starting

--- src/Backend/exercise.java
+++
@@ -1,8 +0,0 @@
-package Backend;
-
-
-public class exercise{
-    public static void main(String [] args){
-    System.out.println("kemon acho");
-    }
-}
--- src/Backend/onlyforrough.java
+++
@@ -1,8 +0,0 @@
-package Backend;
-
-public class onlyforrough {
-
-    public void onlyforrough() {
-        System.out.println(500);
-    }    
-}
