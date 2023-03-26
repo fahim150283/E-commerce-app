4fe3ee61646366e43630b9feac1b1c20110a2181
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-26 17:15:29 +0600

    still creating

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,24 +1,17 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    const expenseDate = new Date(2023, 5, 28);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 587.69;
-
-
-    return (
-        <div className="expense-item">
-            <div className="expense-item__description">{expenseDate.toISOString()}
-            </div>
-            <div>
-                <h2>
-                    {expenseTitle}
-                </h2>
-                <div className = "expense-item__price">                   {expenseAmount}
-                </div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__description">
+        {props.date.toISOString()}
+      </div>
+      <div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price"> {props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem; 
+export default ExpenseItem;
