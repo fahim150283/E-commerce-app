d33992dbe3245a95683e0f0f49b3f4549f720ffb
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-26 16:01:04 +0600

    still creating

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,21 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(){
+    const expenseDate = new Date(2023, 5, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 587.69;
+
+
     return (
         <div className="expense-item">
-            <div className="expense-item__description">1st january 2023</div>
+            <div className="expense-item__description">{expenseDate.toISOString()}
+            </div>
             <div>
-                <h2>Car insurance</h2>
-                <div className="expense-item__price">$ 2300</div>
+                <h2>
+                    {expenseTitle}
+                </h2>
+                <div className = "expense-item__price">                   {expenseAmount}
+                </div>
             </div>
         </div>
     );
