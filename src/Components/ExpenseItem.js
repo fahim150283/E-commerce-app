4b88b92d6936cc8ec0ce3fdd8cd823daf5aac7f9
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-28 22:28:29 +0600

    props working fine

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,26 +1,10 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-// function ExpenseItem() {
-// var title='valobasha';
-// var date= new Date(2023,2,14);
-// var amount= 69;
 
-//   return (
-//     <div className="expense-item">
-//       <div className="expense-item__description">
-//         {date.toISOString()}
-//       </div>
-//       <div>
-//         <h2> {title} </h2>
-//         <div className="expense-item__price"> ${amount} </div>
-//       </div>
-//     </div>
-//   );
   return (
     <div className="expense-item">
       <div className="expense-item__description">
-        {/* {props.date.toISOString()} */}
         {props.date.toISOString().slice(0, 10)}
       </div>
       <div>
