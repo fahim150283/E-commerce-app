377642468445ef1ca5315c288bf246714ee3b417
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-03-28 21:44:29 +0600

    not working the props

--- src/App.js
+++ src/App.js
@@ -3,38 +3,64 @@ import ExpenseItem from "./Components/ExpenseItem";
 function App() {
   const expenses = [
     {
-      id : e1,
-      date : new Date(2023, 5, 28),
-      Title: "Car Insurance",
-      Amount: 587.69,
+      id: 'e1',
+      date: new Date(2023, 5, 28),
+      Title: 'Car Insurance',
+      Amount: 587,
     },
     {
-      id : e2,
-      date : new Date(2003, 4, 8),
-      Title: "Healt Insurance",
-      Amount: 58.39,
+      id: 'e2',
+      date: new Date(2003, 4, 8),
+      Title: 'Healt Insurance',
+      Amount: 58,
     },
     {
-      id : e3,
-      date : new Date(1923, 8, 21),
-      Title: "tanks",
-      Amount: 58787.00,
+      id: 'e3',
+      date: new Date(1923, 8, 21),
+      Title: 'tanks',
+      Amount: 58787,
     },
     {
-      id : e4,
-      date : new Date(2069, 9, 3),
-      Title: "House on the Mars",
-      Amount: 96587.00,
+      id: 'e4',
+      date: new Date(2069, 9, 3),
+      Title: 'House on the Mars',
+      Amount: 96587,
     },
   ];
+
+  
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}> </ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}> </ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}> </ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}> </ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      >
       
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      >
+        
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      >
+    
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      >
+        
+      </ExpenseItem>
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,30 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+// function ExpenseItem() {
+// var title='valobasha';
+// var date= new Date(2023,2,14);
+// var amount= 69;
+
+//   return (
+//     <div className="expense-item">
+//       <div className="expense-item__description">
+//         {date.toISOString()}
+//       </div>
+//       <div>
+//         <h2> {title} </h2>
+//         <div className="expense-item__price"> ${amount} </div>
+//       </div>
+//     </div>
+//   );
   return (
     <div className="expense-item">
       <div className="expense-item__description">
         {props.date.toISOString()}
       </div>
       <div>
-        <h2>{props.title}</h2>
-        <div className="expense-item__price"> {props.amount}</div>
+        <h2> {props.Title} </h2>
+        <div className="expense-item__price"> ${props.Amount} </div>
       </div>
     </div>
   );
